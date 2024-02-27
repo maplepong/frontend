dbfc0386fd135e566816cfcd7fada6d05067da41
siwolee <siwolee@student.42seoul.kr>
2024-02-27 20:27:01 +0900

    Feat: Link(data-route) attribute update, page clean up

---
+++ src/page/simplepage.js
@@ -0,0 +1,15 @@
+export function Home() {
+  return `<h1>TRANSCENDENCE</h1>
+            <p>우당탕탕 초월 대작전</p>`;
+}
+
+export function About() {
+  return `<h1>ABOUT</h1>
+            <p>참여자: 조수빈 정원이 이시원 박경민</p>
+            <p class="fade-move">이주현</p>`;
+}
+
+export function Contact() {
+  return `<h1>CONTACT</h1>
+            <p>연락 안 받습니다</p>`;
+}
