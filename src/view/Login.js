508bf23c6e29eb3a278d99c41dd0c55746b2cde8
siwolee <siwolee@student.42seoul.kr>
2024-02-27 18:28:33 +0900

    Feat: login Component

--- src/view/Login.js
+++ src/view/Login.js
@@ -4,12 +4,18 @@ import Input from "../components/Input.js";
 //상태 확인했는데 로그인할게 없으니까 로그인 페이지로 들어갔겠지...
 //만약 로그인 상태면 home으로 리다이렉트하는 게 맞을듯
 export default class Login extends Component {
-  setup() {}
+  username;
+  password;
+  setup() {
+    this.username = "";
+    this.password = "";
+  }
   template() {
     return `
 		<div id="login-page">
 			<div id="login-id-input"></div>
 			<div id="login-password-input"></div>
+			<button id="login-btn" />
 		</div>
 		`;
   }
@@ -18,15 +24,43 @@ export default class Login extends Component {
     const $idInput = this.$target.querySelector("#login-id-input");
     const $pwInput = this.$target.querySelector("#login-password-input");
 
-    new Input($idInput, { validate: validateId, button: false });
-    new Input($pwInput, { validatePassword, button: false });
+    new Input($idInput, {
+      validate: validateId,
+      label: "ID",
+      placeholder: "당신의ID를겟또다제",
+    });
+    new Input($pwInput, {
+      validate: validatePassword,
+      label: "PW",
+      placeholder: "비밀번호입력구다사이",
+    });
   }
 
   validateId(text) {
     if (text.length < 5) console.log("too short");
   }
 
-  validatePassword(key, target) {
+  validatePassword(text) {
     if (text.length < 5) console.log("too short pw");
+  }
+
+  requestLogin() {
+    this.username = this.$target
+      .querySelector("#login-id-input")
+      .querySelector("input").value;
+    this.password = this.$target
+      .querySelector("#login-password-input")
+      .querySelector("input").value;
+    alert(this.username + this.password + "login success");
+  }
+
+  setEvent() {
+    this.addEvent("keyup", "input", ({ key }) => {
+      if (key != "Enter") return;
+      this.requestLogin();
+    });
+    this.addEvent("click", "button", () => {
+      this.requestLogin();
+    });
   }
 }
