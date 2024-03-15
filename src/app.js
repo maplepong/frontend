9011b16532e66a5607902895b908d8ec5bc65438
siwolee <siwolee@student.42seoul.kr>
2024-03-15 14:43:40 +0900

    connect fin

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import router from "../src/core/Router.js";
 
 
 const setAxios = () => {
-	axios.defaults.baseURL = "http://localhost:8002/";
+	axios.defaults.baseURL = "http://localhost:8000/";
 	// axios.defaults.baseURL = "http://10.19.247.54:8001/";
 	// axios.defauls.timeout = 1000;
 }
@@ -41,54 +41,3 @@ export const App = () => {
 		<main></main>
     `;
 }
-
-
-
-// export default class App extends Component {
-//   setup() {
-//     this.state = {};
-//     document.addEventListener("DOMContentLoaded", () => {
-//       window.addEventListener("routeChange", router);
-//       window.addEventListener("popstate", router);
-
-//       // 초기 페이지 로드 시 라우터 호출
-//       router();
-//     });
-//   }
-
-//   template() {
-//     return `
-// 		<div data-route="">
-//       <img src="./src/asset/design/maplepong.png" id="Logo">
-//     </div>
-//     <header>
-// 			<button data-route="home">Home</button>
-// 			<button data-route="about">About</button>
-// 			<button data-route="contact">Contact</button>
-// 			<button data-route="login" id="LgnContainer">Login</button>
-// 			<button data-route="main">after login</button>
-// 		</header>
-// 		<main></main>
-//     `;
-//   }
-
-//   mounted() {
-// 	setAxios();
-//   }
-
-//   setEvent() {
-//     this.addEvent("click", "[data-route]", ({ target }) => {
-//       const route = target.dataset.route;
-//       if (route) {
-//         const newPath = "/" + route;
-//         history.pushState({}, "", newPath);
-//         router();
-//       } 
-// 	//   else {
-//     //     console.log("Invalid or empty data-route attribute");
-//     //     history.pushState({}, "", "/");
-//     //     router(); // 변경된 URL에 따라 컴포넌트 렌더링
-//     //   }
-//     });
-//   }
-// }
