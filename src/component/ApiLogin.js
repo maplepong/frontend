21efe950549652381a4535e310ea7cf42b8abdab
chobinee <58325848+chobinee@users.noreply.github.com>
2024-03-21 16:30:23 +0900

    feat: api login

--- src/component/ApiLogin.js
+++ src/component/ApiLogin.js
@@ -1,19 +1,42 @@
 /* @jsx createElement */
 import { request42ApiLogin, requestUserInfo } from "../core/Api.js";
 import router from "../core/Router.js";
-import {createElement, Link } from "../core/myReact.js";
+import { createElement, Link } from "../core/myReact.js";
 import "../css/MyInfo.css"
 
-const API= () => {
-	const response = request42ApiLogin();
-
+const ApiLogin = () => {
+	const getCode = () => {
+		const url = window.location.pathname;
+		const code = url.split("code=")[1];
+		console.log("url:", url);
+		return code;
+	}
+	const code = getCode();
+	if (code) {
+		console.log("code:", code);
+		request42ApiLogin(code)
+			.then(response => {
+				console.log("로그인 성공:", response);
+				requestUserInfo()
+					.then(userInfo => {
+						console.log("사용자 정보:", userInfo);
+						router.navigate("/user-info");
+					})
+					.catch(error => {
+						console.error("사용자 정보 요청 중 오류 발생:", error);
+					});
+			})
+			.catch(error => {
+				console.error("로그인 요청 중 오류 발생:", error);
+			});
+	}
+	else
+		console.log("code is null");
 	return (
-		<div id="container-myinfo" class="modal">
-			<div id="myinfo-headline">
-				<p>redirection page</p>
-			</div>
+		<div id="api-login-container">
+			<p>42 login</p>
 		</div>
 	)
 }
 
-export default MyInfo;
+export default ApiLogin;
