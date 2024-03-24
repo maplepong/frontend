4c5feffd624dae8421b90656b3ebb00d6e6d34c6
Espebaum/gyopark <vocops5000@gmail.com>
2024-03-24 17:39:32 +0900

    Feat:checked useState

--- src/component/Home.js
+++ src/component/Home.js
@@ -2,16 +2,8 @@
 import {createElement, Link, render, useState } from "../core/myReact.js";
 
 const Home = () => {
-	const [count, setCount] = useState(1);
-
-	const buttonClick = () => {
-		console.log(typeof count);
-		setCount(count + 1);
-	}
-
 	return (
 		<div id="home">
-			<button onClick={buttonClick}>{count}</button>
 			<Link to="/" id=""></Link>
 			<div id="character"></div>
 		</div>
