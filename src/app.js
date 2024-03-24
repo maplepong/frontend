a9340dd3722fd01a3ac309cab109b3e634e63ba4
Espebaum/gyopark <vocops5000@gmail.com>
2024-03-24 17:33:18 +0900

    Feat:Checking React functions

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 /* @jsx createElement */
 import Login from "./component/Login.js";
 import MyInfo from "./component/MyInfo.js";
-import { createElement,Link } from "./core/myReact.js";
+import { createElement, Link } from "./core/myReact.js";
 import SignUp from "./component/SignUp.js";
 import "./css/index.css";
+import { useState } from "./core/myReact.js";
 
 const setAxios = () => {
 	axios.defaults.baseURL = "http://localhost:8000/";
@@ -12,21 +13,36 @@ const setAxios = () => {
 }
 const App = () => {
 	setAxios();
+    const [ count, setCount ] = useState(1);
+    
+	const incre = () => {
+		// console.log(typeof count);
+		setCount(count() + 1);
+	}
+	const decre = () => {
+		// console.log(typeof count);
+		setCount(count() - 1);
+	}
+
 	return (
-	<div class="app">
-		<Link to= "">
-			<img src ="./asset/design/maplepong.png"></img>
-		</Link>
-		<h1>
-			Merancendance
-		</h1>
-		<Login />
-		<MyInfo />
-		<SignUp />
-		<button data-route="home">Home</button>
-		<button data-route="login">login</button>
-		<button data-route="signup">SignUp</button>
-	</div>
+        <div class="app">
+            <Link to= "">
+                <img src ="./asset/design/maplepong.png"></img>
+            </Link>
+            <p>Test UseState: {count()}
+                <button onClick={() => incre()}>증가</button>
+                <button onClick={() => decre()}>감소</button>
+            </p>
+            <h1>
+                Merancendance
+            </h1>
+            <Login />
+            <MyInfo />
+            <SignUp />
+            <button data-route="home">Home</button>
+            <button data-route="login">login</button>
+            <button data-route="signup">SignUp</button>
+	    </div>
 	)
 }
 
--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ import App from "./app.js"
 import Navbar from "./component/Navbar.js";
 
 const root = MyReactDOM.createRoot(document.querySelector("#root"));
+
 root.render(<div>
 	<App />
 	<Navbar />
-	</div>);
+	</div>
+);
