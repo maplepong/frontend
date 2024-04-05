3d8375a6c7b91d8c2cb97dfd9250362691cd3bea
siwolee <siwolee@student.42seoul.kr>
2024-04-06 00:40:54 +0900

    test: now setState one time fine

--- src/core/fiberNode.js
+++ src/core/fiberNode.js
@@ -1,32 +1,50 @@
 export default class fiberNode {
 	constructor() {
-        this.instance = null; // The component instance
-        this.children = []; // Child component
-        this.sibling = []; // Sibling component
+		this.tag = "";
+		this.props = {};
+		this.children = []; // Child component
+       
+		this.sibling = []; // Sibling component
         this.return = null; // Parent component
         this.stateNode = null; // Associated DOM node or component state
 		this.state = [];
 		this.position = 0;
+		this.instance = null;
+		this.changed = false;
+		this.changedState = [];
     }
+	getInfo(oldFiber){
+		this.stateNode = oldFiber.stateNode;
+		this.state = oldFiber.state;
+		this.instance = oldFiber.instance;
+		this.tag = oldFiber.tag;
+		this.props = oldFiber.props;
+		this.position = 0;
+		this.changedState = oldFiber.changedState;
+		oldFiber.changedState = [];
+		this.changed = oldFiber.changed;
+		oldFiber.changed = false;
+	}
+
     render(parentNode) { // setting fiberNode values, not real rendering
-	    if (!stateNode) { // first Render
-		    this.stateNode = document.createElement(this.instance[type]);
-			if (!parentNode){ //rootNode
-				this.return = null;
-			}
-			else { //부모 노드에 대한 처리
-				this.return.return.appendChild(this.stateNode);
-				this.return = parentNode;
-				parentNode.children.array.forEach(element => {
-					this.child.append(element);
-					element.sibling.append(this);
-				});
-			}
-			//자식 노드에 대한 처리
-			this.instance.children.forEach((child) => {
-				this.children.append(child.render(this));
-			})
-			return this;
-		}
+	// if (!stateNode) { // first Render
+	 //부모 노드에 대한 처리
+	// this.return.return.appendChild(this.stateNode);??
+	this.return = parentNode;
+	parentNode.children.array.forEach(element => {
+		this.child.append(element);
+		element.sibling.append(this);
+	});
+
+	//자식 노드에 대한 처리
+	this.instance.children.forEach((child) => {
+		this.children.append(child.render(this));
+		console.log(this.children)
+	})
+	return this;
+		// }
+	}
+	compare(fiber){
+		
 	}
 }
--- src/core/myReact.js
+++ src/core/myReact.js
@@ -1,5 +1,6 @@
 import fiberNode from "./fiberNode.js";
-import { exist } from "./utils.js"
+import myReactDOM from "./myReactDOM.js";
+import { exist, isEmptyObj } from "./utils.js"
 
 export function Link(props){
 	// console.log(props)
@@ -26,54 +27,143 @@ function makeProps(props, children){
 }
 
 const myReact = {
-	enrenderComponent : [], //component to render, added when useState/setState calls.
-	enrenderQueue : [], // 
+	enrenderComponent : [],
+	enrenderQueue : [], //what is changed {changedhow, component, props}
 	callback : [], // 
-	virtualDOM : null,  //root of fiberNode
+	fiberRoot : null,  //root of fiberNode
 	currentFiberNode : null,
 
 	render : async function render(newVirtualDOM){
-		position = 0;
-		if (!this.VirtualDOM) { //first Render
-			this.virtualDOM = newfiberNode.render();
-			await myReactDOM.render(this.virtualDOM);
+		if (!this.fiberRoot) { //first Render, called by DOM
+			this.fiberRoot = newVirtualDOM;
+			await myReactDOM.root.initDOM(this.fiberRoot);
 		}
-		else {
-			diffDOM(newVirtualDOM, this.virtualDOM);
-			await myReactDOM.reconciliation(this.enrenderQueue);
-			this.enrenderQueue = [];
+		else {//변화하는 경우 : 이게 enrenderQueue에 들어가있을때밖에 없나???
+			//re-render....
+			//is it reRender or 
+		const newFiberRoot = this.reRender(this.fiberRoot);
+		// this.diffRoot(this.FiberRoot); //enrenderQueue를 통해 바뀐 부분 찾기
+		// await myReactDOM.updateDOM(this.enrenderQueue); //나중에 바뀐 부분만 보낼 수 있게...업데이트 할 수 있으면...좋고...
+		await myReactDOM.root.updateDOM(newFiberRoot);
+		this.fiberRoot = newFiberRoot;
+		this.enrenderQueue = [];
 		}
 		this.callback.forEach((f) => f());
 	},
+
+	erase : function erase() {
+		removeFiberRoot();
+		this.enrenderComponent = []
+		this.enrenderQueue = []
+		this.callback = []
+	},
 	
 	createElement : function createElement(tag, props, ...children){
-		const vNode = new fiberNode();
-		this.currentFiberNode = vNode; //tracking which Fiber is on rendering
+		const fiber = new fiberNode();
+		var instance;
+		window.currentFiberNode = fiber; //tracking which Fiber is on rendering
 		props = props || {};
 		children = children || [];
 		if (typeof tag === 'function'){
+			fiber.instance = tag;
 			if (children.length > 0){
 				props = makeProps(props, children)
 			}
-			vNode.instance = (tag(props));
+			instance = (tag(props));
 		}
 		else{
-			vNode.instance = {tag, props, children};
+			instance = {tag, props, children};
 		}
-		this.currentFiberNode = null;
-		return vNode;
+		fiber.tag = instance.tag;
+		fiber.props = instance.props;
+		fiber.children = instance.children;
+		window.currentFiberNode = null;
+		return fiber;
 	},
+
+	// code: updates fiber by call instance function
+	// reRender : function (fiber) {
+	// 	if (fiber === undefined) return;
+	// 	if (fiber.changed === true){
+	// 		window.currentFiberNode = fiber;
+	// 		fiber.position = 0;
+	// 		const instance = fiber.instance(fiber.props);
+	// 		//삭제해야하는거 챙겨야할수도??
+	// 		fiber.tag = instance.tag;
+	// 		fiber.props = instance.props;
+	// 		fiber.children = instance.children;
+	// 		fiber.changed = false;
+	// 		window.currentFiberNode = null;
+	// 	}
+	// 	else {
+	// 		fiber.children.forEach((child) => {
+	// 			this.reRender(child);
+	// 		})
+	// 	}
+	// },
+	reRender : function (oldfiber) {
+		const fiber = new fiberNode();
+		//new fiber state value update && copy values of old fiber
+		//if fiber changed? call instance
+		console.log("state", fiber.state);
+		fiber.getInfo(oldfiber);
+		if (fiber.changed){
+			console.log("changedState", fiber.changedState)
+			fiber.changedState.forEach(d => {
+				console.log(d)
+				fiber.state[d.i] = d.value;
+				console.log("fiber updated state" , fiber.state);
+			})
+			fiber.changedState = [];
+			fiber.changed = false;
+			window.currentFiberNode = fiber;
+			const instance = fiber.instance(fiber.props);
+			fiber.tag = instance.tag;
+			fiber.props = instance.props;
+			fiber.children = instance.children;
+			window.currentFiberNode = null;
+		}
+		else {
+			oldfiber.children.forEach((child) => {
+				if (typeof child === "number" || typeof child === "string"){
+					fiber.children.push(child);
+				}
+				else {
+					fiber.children.push(this.reRender(child));
+				}
+
+			})
+		}
+		console.log(fiber);
+		return fiber;
+	},
+
+	/* 현재의 fiberRoot, newFiberRoot 비교해서
+	만약 바뀐 부분 있으면 enrenderQueue에 추가
+	전부 비교한 후 Root 대체
+
+	*/
+	diffRoot : function (newFiberRoot) {
+		this.fiberRoot = newFiberRoot;
+	}
 }
 
 export function useState(initValue){
-	const fiber = myReact.currentFiberNode;
+	const fiber = window.currentFiberNode;
 	const i = fiber.position;
 	fiber.position++;
-	fiber.state[i] = fiber.state[i] || initValue; 
+	// console.log("useState", fiber.state[i]);
+	fiber.state[i] = fiber.state[i] || initValue;
 	const setState = (value) => {
 		if (fiber.state[i] === value)
 			return console.log("setState err-value same-",value);
-		fiber.state[i] = value;
+		fiber.changedState.push({i, value});
+		myReact.enrenderComponent.push(fiber);
+		// myReact.enrenderQueue.append(["stateChange", fiber, i]);
+		fiber.changed = true;
+		myReact.render();
+		//render, how I can get the infomation of current page?
+		// console.log("setState", fiber);
 	}
 	return [fiber.state[i], setState];
 }
