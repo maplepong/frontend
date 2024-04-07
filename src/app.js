17d776cf5cd02911a4759e0c6904e22539d4160b
Juhyun Lee <juhyulee@c2r6s3.42seoul.kr>
2024-04-07 19:27:41 +0900

    feat: new game page

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import Login from "./component/Login.js";
 import MyInfo from "./component/MyInfo.js";
 import { createElement,Link } from "./core/myReact.js";
 import SignUp from "./component/SignUp.js";
+import Game from "./component/Game.js"
 import "./css/index.css";
 
 const setAxios = () => {
@@ -10,22 +11,20 @@ const setAxios = () => {
 	// axios.defaults.baseURL = "http://10.19.247.54:8001/";
 	// axios.defauls.timeout = 1000;
 }
+
 const App = () => {
 	setAxios();
+
+	const updateScore = (score) => {
+		const scoreElement = document.querySelector('#score p');
+		scoreElement.textContent = score;
+	};
+
 	return (
 	<div class="app">
-		<Link to= "">
-			<img src ="./asset/design/maplepong.png"></img>
+		<Link to="/game">
+			<button id="linkGame">to game</button>
 		</Link>
-		<h1>
-			Merancendance
-		</h1>
-		<Login />
-		<MyInfo />
-		<SignUp />
-		<button data-route="home">Home</button>
-		<button data-route="login">login</button>
-		<button data-route="signup">SignUp</button>
 	</div>
 	)
 }
---
+++ src/component/GameContainer.js
@@ -0,0 +1,25 @@
+/* @jsx createElement */
+import {createElement} from "../core/myReact.js";
+import Game from "./Game.js"
+import "../css/Pingpong.css"
+
+const GameContainer = () => {
+	// const updateScore = (score) => {
+	// 	const scoreElement = document.querySelector('#score p');
+	// 	scoreElement.textContent = score;
+	// };
+
+	return (
+	<div class="game-container">
+		<canvas id="myCanvas" >
+		</canvas>
+		<div id="score">
+			<p>0:0</p>
+		</div>
+		<button id="runButton">Start game</button>
+		<button id="" onclick={Game}>Render</button>
+	</div>
+	)
+}
+
+export default GameContainer
