1e63f457868ef34299cff536782dd024fbc47f1e
siwolee <siwolee@student.42seoul.kr>
2024-04-11 20:14:26 +0900

    feat: get info and show

--- src/component/MyInfo.js
+++ src/component/MyInfo.js
@@ -1,24 +1,47 @@
 /* @jsx myReact.createElement */
 import { requestUserInfo } from "../core/Api.js";
-import myReact , { Link, useEffect } from "../core/myReact.js";
+import myReact , { Link, useEffect, useState} from "../core/myReact.js";
 import "../css/MyInfo.css"
 
 const MyInfo = () => {
-	const resultInfo = (data) => {
-		if (!data)
+	const [data, setData] = useState({
+		id: "",
+		username: "",
+		nickname: "",
+		introduction: "",
+		losses: "",
+		total_games: "",
+		wins: "",
+		win_rate: "",
+		image: "",
+	});
+	const resultInfo = (responsedata) => {
+		if (!responsedata)
 			return console.error("noDATA")
-		console.log(data);
+		console.log(responsedata);
+		setData({...responsedata});		
+		console.log(responsedata.image)
 	}
 	useEffect(() => requestUserInfo(localStorage.nickname, resultInfo), []);
+	// useEffect(() => console.log(data), [data]);
 	return (
 		<div id="container-myinfo" class="modal">
 			<div id="myinfo-headline">
-				<p></p>
+				<p>내정보</p>
 				<button>X</button>
 			</div>
-			<div id="myinfo-body">
-				<img id="myinfo-img"></img>
-				<div></div>
+			<div id="myinfo-body" onclick={() => console.log(data)}>
+				<img id="myinfo-img" src={data.image}></img>
+				<ul>
+					<li>id {data.id}</li>
+					<li>information {data.introduction}</li>
+					<li>lose {data.losses}</li>
+					<li>nickname {data.nickname}</li>
+					<li>totalgame {data.total_games}</li>
+					<li>username {data.username}</li>
+					<li>wins {data.wins}</li>
+					<li>win_rate {data.win_rate}</li>
+				</ul>
 			</div>
 		</div>
 	)
