576c37f8d279c42d069c37f765678fa5408313c6
haecho <fimicat@gmail.com>
2024-04-11 17:34:41 +0900

    feat: on-event update

--- src/component/Test.js
+++ src/component/Test.js
@@ -4,46 +4,55 @@ import myReact, { useState, useEffect, Link } from "../core/myReact.js";
 import router from "../core/Router.js"
 
 const Test = () => {
+
+	//test: count state
     const [count, setCount] = useState(0);
-    
 	const incre = () => {
 		setCount(count + 1);
 	}
 	const decre = () => {
 		setCount(count - 1);
 	}
-
-	const [text, setText] = useState("test");
-	const testChange = (e) => {
-		e.preventDefault();
-		console.log("textInput", document.querySelector("#textInput").value);
-		setText(document.querySelector("#textInput").value);
-	}
-
 	const callbackTest = () => {
 		console.log("callback : callllllllbackkkkkkk");
 	}
 	useEffect(callbackTest, [count]); //count 변수 변경시 실행
 
+	//test: text state
+	const [text, setText] = useState("test");
 	useEffect(function testDep() {
 		console.log("callback : test Dependencies")
 	}, [])
 	const callback = () => {
 		console.log("callback : text change")
 	}
-	const re = () => {
-		myReact.redirect("welcome");
+	useEffect (callback, [text]); // 텍스트 변수 변경시 실행
+	const textchange = () => {
+		setText(document.querySelector("#textInput").value)
 	}
-	useEffect (callback); // 텍스트 변수 변경시 실행
+	
+	//redirect example
+	const re = () => { myReact.redirect("welcome");}
+
+
+	//onClick말고다른거Test
+	const onOtherTest = () => { console.log("onchange success")}
+
 	return <div class="test">
             <p>Test UseState: {count}
-                <button onClick={incre}>증가</button>
-                <button onClick={decre}>감소</button>
+                <button onclick={incre}>증가</button>
+                <button onclick={decre}>감소</button>
             </p>
 			<div>
 				<input id="textInput"></input>
-				<button onCilck={testChange}></button>
-				<p>Text is{text}</p>
+				<button onclick={textchange}></button>
+				<p >Text is{text}</p>
+			</div>
+			<div>
+				<select onchange={onOtherTest}>
+					<option value="ok">ok</option>
+					<option value="no">no</option>
+					</select>
 			</div>
 			<button onClick={re}>Test redirect</button>
 	    </div>
