cf44affce0a2972d82fdf0df27f2941563ae7e5c
Espebaum <vocops5000@gmail.com>
2024-04-12 17:27:10 +0900

    Feat: request Friend

--- src/component/RequestFriend.js
+++ src/component/RequestFriend.js
@@ -6,28 +6,25 @@ import "../css/index.css";
 import "../css/friend.css";
 
 const RequestFriend = () => {
-    requestLogin(() => { return (["subcho", "1234"])}, ()=>{} );
+    // requestLogin(() => { return (["gyopark", "1234"])}, ()=>{} );
 
     const [nickname, setNickname] = useState('');
     // const nickname = "gyopark";
     const handleClick = (event) => {
         event.preventDefault();  // 폼의 기본 동작 방지
-        try {
-            const response = requestFriend(nickname);
-                console.log('친구 요청 결과:', response);
-        } catch (error) {
-            console.error('친구 요청 에러:', error);
-        }
+        const response = requestFriend(nickname);
+        console.log('친구 요청 결과:', response);
     }
 
     return (
         <div id="requestFriend">
             <form id="requestBox">
-                <input type="text" id="nickname" placeholder="닉네임 입력" value={nickname} onchange={e => {
-                    console.log(e);
-                    setNickname(e.target.value)}
-                } />
-                <button type="button" onclick={handleClick}>친구 요청 보내기</button>
+            <input type="text" id="nickname" placeholder="닉네임 입력" value={nickname} onchange={e => {
+                // console.log("Event:", e);
+                console.log("Input value:", e.target.value);
+                setNickname(e.target.value);
+            }} />
+            <button type="button" onclick={handleClick}>친구 요청 보내기</button>
             </form>
         </div>
     );
