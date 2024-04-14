2a4e906f659e36282e85e5cb937440b6e2942f0d
haecho <fimicat@gmail.com>
2024-04-14 14:21:10 +0900

    chore: old folder remove::_core, _component

--- src/component/SignUp.js
+++ src/component/SignUp.js
@@ -1,7 +1,6 @@
 /* @jsx myReact.createElement */
-import myReact , { Link, useEffect } from "../core/myReact.js";
+import myReact from "../core/myReact.js";
 import { requestSignup, requestValidCheck } from "../core/Api";
-import router from "../core/Router.js";
 
 const SignUp = () => {
     const getInfo = () => {
@@ -9,12 +8,10 @@ const SignUp = () => {
 		const password = document.querySelector("#new-password").value;
         const nickname = document.querySelector("#new-nickname").value;
         requestSignup(username, password, nickname);
-        history.pushState({}, "", "/home");
 	};
 
     return (
         <div id="Signup-container">
-            
             <input id="new-username" 
             placeholder="ID"></input>
 			<input id="new-password" placeholder="password"></input>
