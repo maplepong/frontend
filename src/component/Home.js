7dfd6ad8cf331cdf5d89cf8aed8edf3ccc2bd48e
Espebaum <vocops5000@gmail.com>
2024-05-08 19:08:56 +0900

    feat: building home

--- src/component/Home.js
+++ src/component/Home.js
@@ -3,9 +3,11 @@ import myReact , { Link } from "../core/myReact.js";
 import { useState } from "../core/myReact.js";
 import Navbar from "./Navbar.js";
 import FriendList from "./FriendList.js";
+import ChooseGame from "./ChooseGame.js";
 import Modal from "./Modal.js";
 import "../css/home.css"
 import UserInfo from "./UserInfo.js";
+import UserStatus from "./UserStatus.js";
 
 const Home = () => {
 
@@ -24,13 +26,17 @@ const Home = () => {
     // };
 
 	return <div id="home">
-			{/* 아래 라인 주석 치면 에러안남 */}
-			{/*<Modal content={modalContent} onClose={hideModal} /> && modalVisible */}
-			<FriendList callback={showModal}/>
-			{/* <UserInfo class={status.class} nickname={status.nickname}></UserInfo> */}
-			<div id="info">
-			</div>
-			{/* <Navbar /> */}
+				{/* 아래 라인 주석 치면 에러안남 */}
+				{/*<Modal content={modalContent} onClose={hideModal} /> && modalVisible */}
+				<ChooseGame />
+				<div id="myStatus">
+					<UserStatus username={localStorage.username}></UserStatus>
+					<FriendList callback={showModal}/>
+				</div>
+				{/* <UserInfo class={status.class} nickname={status.nickname}></UserInfo> */}
+				<div id="info">
+				</div>
+				{/* <Navbar /> */}
 		</div>
 }
 
