4f62d75a0d8643fd021de0110fb33fbb8c79ed75
Espebaum/gyopark <vocops5000@gmail.com>
2024-05-13 17:37:23 +0900

    feat: see friendinfo

--- src/component/NicknameHover.js
+++ src/component/NicknameHover.js
@@ -20,14 +20,14 @@ const NicknameHover = ({nickname}) => {
 		const menu = document.querySelectorAll(".hovermenu");
 		menu.classList.add("hidden");
 	}
-	function routeToFriend() {
-		router("/userinfo");
+	function routeToFriend(nickname) {
+		myReact.redirect("userinfo/"+nickname);
 	}
 	return <div class="nicknameContainer" id={nickname} onclick={showmenu} > 
 		<p>{nickname}</p>
 		<div class="hidden hovermenu">
 			<ul>
-				<li><span onclick={routeToFriend}>{nickname} 정보보기</span></li>
+				<li><span onclick={()=>{ (routeToFriend(nickname))}}>{nickname} 정보보기</span></li>
 				<li>친구신청하기</li>
 				<li>게임초대하기</li>
 				<li>대화하기</li>
