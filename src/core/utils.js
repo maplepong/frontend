89cee657d86e6c158e631251a02d9eb8b3972ef5
siwolee <siwolee@student.42seoul.kr>
2024-05-24 22:51:50 +0900

    test: object compare in state

--- src/core/utils.js
+++ src/core/utils.js
@@ -33,7 +33,17 @@ export function isEqualArray (a, b) {
 	if (a.length === 0 && b.length === 0)
 		return true;
 	return a.length && b.length && 
-		a.every((v, i) => v === b[i])
+		a.every((v, i) => {
+			if (typeof v === "object" && typeof b[i] === "object"){
+				console.log("----------------------obj compare called----------");
+				console.log(Object.entries(v).toString() );
+				console.log(Object.entries(b[i]).toString() );
+				return (Object.entries(v).toString() === Object.entries(b[i]).toString());
+			}
+			if (typeof v === "object" || typeof b[i] === "object")
+				return false ;
+			return (v === b[i])
+		})
 }
 
 export function makeProps(props, children){
