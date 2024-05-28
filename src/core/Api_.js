417fb854cff722595ade520090344854fe6f1472
chobinee <58325848+chobinee@users.noreply.github.com>
2024-05-28 19:24:42 +0900

    fix: gameRoom

--- src/core/Api_.js
+++ src/core/Api_.js
@@ -2,7 +2,7 @@ import myReact from "./myReact";
 import axios from "axios";
 
 const apiInstance = axios.create({
-	baseURL: "http://localhost:9000/",
+	baseURL: "http://localhost:8000/",
 	headers: {
 		'Content-Type' : 'application/json',
 	},
