5e0624cb8c1053f18932861ee31f362f519273ca
chobinee <58325848+chobinee@users.noreply.github.com>
2024-06-02 01:19:22 +0900

    fix: game

--- src/component/Chat.js
+++ src/component/Chat.js
@@ -1,41 +1,56 @@
 /* @jsx myReact.createElement */
-import myReact , { useEffect, useState } from "../core/myReact.js";
-import '../css/Chat.css'
+import myReact, { useEffect, useState } from "../core/myReact.js";
+import '../css/Chat.css';
 
-const Chat = ( {socket} ) => {
-	const [messages, setMessages] = useState([]);
+const Chat = ({ gameId }) => {
+    const [messages, setMessages] = useState([]);
+    const [chatSocket, setChatSocket] = useState(null);
 
-	useEffect(() => {
-		if (socket){
-			socket.onmessage = (event) => {
-				const data = JSON.parse(event.data);
-				console.log("data : ", data);
-				if (data.type === "chat") {
-					const message = data.message;
-					const sender = data.nickname;
-					setMessages([...messages, { sender, message }]);
-				}
-			}
-		}
-	}, []);
+    useEffect(() => {
+        if (gameId && !chatSocket) {
+            const newChatSocket = new WebSocket("ws://localhost:8000/ws/chat/" + gameId + "/");
+            setChatSocket(newChatSocket);
 
-    const socket_chat_message = (data) => {
-        const message = data.message;
-        const sender = data.nickname;
-        setMessages([...messages, { sender, message }]);
-    }
-
-	const sendMessage = (message) => {
-        if (socket) {
-            socket.send(JSON.stringify({ type: "chat", message: message, nickname: localStorage.getItem("nickname") }));
+            newChatSocket.onopen = () => {
+                console.log("chat socket opened");
+                newChatSocket.send(JSON.stringify({ type: "connect", message: "hello", nickname: localStorage.getItem("nickname") }));
+            };
         }
-		else
-			alert("socket이 연결되지 않았습니다.");
+        if (chatSocket) {
+            chatSocket.onmessage = (event) => {
+                const data = JSON.parse(event.data);
+                console.log("chat data :", data);
+                if (data.type === "chat") {
+                    const message = data.message;
+                    const sender = data.nickname;
+                    setMessages((prevMessages) => [...prevMessages, { sender, message }]);
+                }
+            };
+
+            chatSocket.onclose = () => {
+                console.log("채팅 연결 종료");
+                setChatSocket(null);
+            };
+        }
+            return () => {
+                if (chatSocket)
+                {   chatSocket.close();
+                    setChatSocket(null);
+                }
+            };
+    }, [chatSocket, gameId]);
+
+    const sendMessage = (message) => {
+        if (chatSocket && chatSocket.readyState === WebSocket.OPEN) {
+            chatSocket.send(JSON.stringify({ type: "chat", message: message, nickname: localStorage.getItem("nickname") }));
+        } else {
+            alert("socket이 연결되지 않았습니다.");
+        }
     };
 
-	return (
+    return (
         <div id="container-chat">
-            <div id="chat-list"> 
+            <div id="chat-list">
                 <button>전체</button>
             </div>
             <div id="chat">
@@ -47,15 +62,14 @@ const Chat = ( {socket} ) => {
                     ))}
                 </div>
             </div>
-			<input type="text" onKeyDown={(e) => {
-                    if (e.key === 'Enter') {
-                        sendMessage(e.target.value);
-                        e.target.value = '';
-                    }
-                }} />
+            <input type="text" onKeyDown={(e) => {
+                if (e.key === 'Enter') {
+                    sendMessage(e.target.value);
+                    e.target.value = '';
+                }
+            }} />
         </div>
     );
 }
-
 
 export default Chat;
