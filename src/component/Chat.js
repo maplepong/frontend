bb5869f706dedea22544afe2e580afe53b868284
siwolee <siwolee@student.42seoul.kr>
2024-06-13 18:12:08 +0900

    feat: now I can see chat

--- src/component/Chat.js
+++ src/component/Chat.js
@@ -1,96 +1,103 @@
 /* @jsx myReact.createElement */
-import myReact, { useEffect, useState } from "../core/myReact.js";
-import '../css/Chat.css';
+import myReact, { useEffect, useRef, useState } from "../core/myReact.js";
+import "../css/Chat.css";
 
-const Chat = ({ gameId }) => {
-    const [messages, setMessages] = useState([]);
-    const [chatSocket, setChatSocket] = useState(null);
+const Chat = () => {
+  const [messages, setMessages] = useState([]);
+  const chatSocket = useRef(null);
 
-	console.log(chatSocket);
-    useEffect(() => {
-		console.log(chatSocket);
-        if (gameId && !chatSocket) {
-            const newChatSocket = new WebSocket("ws://localhost:8000/ws/chat/" + gameId + "/");
-            setChatSocket(newChatSocket);
-            newChatSocket.onopen = () => {
-                console.log("chat socket opened");
-                newChatSocket.send(JSON.stringify({ type: "connect", message: "hello", nickname: localStorage.getItem("nickname") }));
-            };
-
-        }
-		else if (!chatSocket){
-			console.log(ws);
-			const ws = new WebSocket(`ws://localhost:8000/ws/chat/?token=${localStorage.getItem("accessToken")}`);
-			ws.onopen = () => {
-                console.log("chat socket opened");
-                ws.send(JSON.stringify({ type: "connect", message: "hello", nickname: localStorage.getItem("nickname") }));
-            };
-			ws.onmessage = function (event) {
-				console.log(event);
-				const data = JSON.parse(event.data);
-				console.log(data);
-				const message = data.message;
-				const sender = data.sender;
-				const whisper = data.whisper || false;
-				const displayMessage = whisper ? `[Whisper] ${sender}: ${message}` : `${sender}: ${message}`;
-				chatArea.value += displayMessage + '\n';
-			};
-			setChatSocket(ws); 
-		}
-        // if (chatSocket) {
-        //     chatSocket.onmessage = (event) => {
-        //         const data = JSON.parse(event.data);
-        //         console.log("chat data :", data);
-        //         if (data.type === "chat") {
-        //             const message = data.message;
-        //             const sender = data.nickname;
-        //             setMessages((prevMessages) => [...prevMessages, { sender, message }]);
-        //         }
-        //     };
-
-        //     chatSocket.onclose = () => {
-        //         console.log("채팅 연결 종료");
-        //         setChatSocket(null);
-        //     };
-        // }
-            return () => {
-                if (chatSocket)
-                {   chatSocket.close();
-                    setChatSocket(null);
-                }
-            };
-    }, []);
-
-    const sendMessage = (message) => {
-        if (chatSocket && chatSocket.readyState === WebSocket.OPEN) {
-            chatSocket.send(JSON.stringify({ type: "chat", message: message, nickname: localStorage.getItem("nickname") }));
-        } else {
-            alert("socket이 연결되지 않았습니다.");
-        }
+  console.log(chatSocket.current);
+  useEffect(() => {
+    console.log(chatSocket.current);
+    if (!chatSocket.current) chatSocket.current = initSocket();
+    else {
+      chatSocket.current.onmessage = (event) => getMessage(event);
+    }
+    return () => {
+      if (chatSocket.current) {
+        chatSocket.current.close();
+        chatSocket.current = null;
+      }
     };
+  });
 
-    return (
-        <div id="container-chat">
-            <div id="chat-list">
-                <button>전체</button>
-            </div>
-            <div id="chat">
-                <div id="messages">
-                    {messages.map((msg, index) => (
-                        <div key={index}>
-                            <strong>{msg.sender}:</strong> {msg.message}
-                        </div>
-                    ))}
-                </div>
-            </div>
-            <input type="text" onKeyDown={(e) => {
-                if (e.key === 'Enter') {
-                    sendMessage(e.target.value);
-                    e.target.value = '';
-                }
-            }} />
-        </div>
+  function initSocket() {
+    const ws = new WebSocket(
+      `ws://localhost:8000/ws/chat/?token=${localStorage.getItem(
+        "accessToken"
+      )}`
     );
-}
+    ws.onopen = () => {
+      console.log("chat socket opened");
+      ws.send(
+        JSON.stringify({
+          type: "connect",
+          message: "hello",
+          username: localStorage.getItem("username"),
+        })
+      );
+    };
+    ws.onmessage = (event) => getMessage(event);
+    ws.onclose = () => {
+      console.log("채팅 연결 종료");
+      ws = null;
+    };
+    return ws;
+  }
+  const getMessage = (event) => {
+    const data = JSON.parse(event.data);
+    console.log("chat data :", data);
+    // if (data.type === "chat") {
+    setMessages([
+      ...messages,
+      { sender: data.nickname, message: data.message },
+    ]);
+  };
+
+  const sendMessage = (message) => {
+    if (
+      chatSocket.current &&
+      chatSocket.current.readyState === WebSocket.OPEN
+    ) {
+      chatSocket.current.send(
+        JSON.stringify({
+          type: "chat",
+          message: message,
+          username: localStorage.getItem("username"),
+        })
+      );
+      chatSocket.current.onmessage = (event) => getMessage(event);
+    } else {
+      alert("socket이 연결되지 않았습니다.");
+    }
+  };
+  console.log(messages);
+
+  return (
+    <div id="container-chat">
+      <div id="chat-list">
+        <button>전체</button>
+      </div>
+      <div id="chat">
+        <div id="messages">
+          {messages.map((msg, index) => (
+            <div key={index}>
+              <strong>{msg.sender}:</strong> {msg.message}
+            </div>
+          ))}
+        </div>
+      </div>
+      <input
+        type="text"
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            sendMessage(e.target.value);
+            e.target.value = "";
+          }
+        }}
+      />
+    </div>
+  );
+};
 
 export default Chat;
