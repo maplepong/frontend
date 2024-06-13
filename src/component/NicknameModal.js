50ca0c1dd26710c141fbea3e42e833151a3cdafd
siwolee <siwolee@student.42seoul.kr>
2024-06-13 19:30:13 +0900

    feat: added functions to modal

--- src/component/NicknameModal.js
+++ src/component/NicknameModal.js
@@ -1,6 +1,8 @@
 /* @jsx myReact.createElement */
+import api from "../core/Api_.js";
 import myReact, { Link } from "../core/myReact.js";
 import "../css/NicknameModal.css";
+import RequestFriend from "./RequestFriend";
 
 const NicknameModal = ({ nickname }) => {
   var show = 0; //0 hidden 1 show;
@@ -13,9 +15,24 @@ const NicknameModal = ({ nickname }) => {
     else menu[0].classList.add("hidden");
     show = show ? 0 : 1;
   }
-  function routeToFriend(nickname) {
+  function routeToFriend() {
     myReact.redirect("userinfo/" + nickname);
   }
+  async function requestFriend() {
+    const response = await api.sendFriendRequest(nickname);
+    console.log(response.);
+    switch (response.status){
+      case 201 :
+        console.log("")
+    }
+  }
+  function inviteToGame() {
+    console.log("게임초대아직미구현임");
+  }
+  function wisper() {
+    console.log("1on1");
+  }
+
   return (
     <div class="nicknameContainer" id={nickname} onclick={showmenu}>
       <p class="nickname">{nickname}</p>
@@ -24,15 +41,33 @@ const NicknameModal = ({ nickname }) => {
           <li>
             <span
               onclick={() => {
-                routeToFriend(nickname);
+                routeToFriend();
               }}
             >
               🔍 {nickname} 정보보기
             </span>
           </li>
-          <li>👐 친구신청하기</li>
-          <li>🏓 게임초대하기</li>
-          <li>💌 대화하기</li>
+          <li
+            onclick={() => {
+              requestFriend();
+            }}
+          >
+            👐 친구신청하기
+          </li>
+          <li
+            onclick={() => {
+              inviteToGame();
+            }}
+          >
+            🏓 게임초대하기
+          </li>
+          <li
+            onclick={() => {
+              wisper();
+            }}
+          >
+            💌 대화하기
+          </li>
         </ul>
       </div>
     </div>
